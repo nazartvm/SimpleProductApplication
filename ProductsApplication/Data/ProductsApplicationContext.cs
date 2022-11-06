d9b25d8ce496bb4e7639e72f21fe99d29fce3d05
nazartvm <nazar@aktrea.com>
2022-11-06 20:58:25 +0530

    first commit

---
+++ ProductsApplication/Data/ProductsApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductsApplication.Models;
+
+namespace ProductsApplication.Data
+{
+    public class ProductsApplicationContext : DbContext
+    {
+        public ProductsApplicationContext (DbContextOptions<ProductsApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ProductsApplication.Models.Product> Product { get; set; } = default!;
+    }
+}
